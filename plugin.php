6a193fba778913bc00cc9d83ae8aff6fbf3a19ac
satollo <satollo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-01 19:15:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/access-logs/trunk@37562 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Plugin Name: Access Logs
+Plugin URI: http://www.satollo.com/wordpress/access-logs
+Description: Create access log in the Apache combine format. The logs are store in wp-content/logs.
+Version: 1.0
+Author: Satollo
+Author URI: http://www.satollo.com
+Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
+*/
+
+/*	Copyright 2008  Satollo (email : satollo@gamil.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+function aal_activate()
+{
+	mkdir(dirname(__FILE__) . '/../../logs');
+}
+
+function aal_init()
+{
+	global $aal_options;
+	$href = $_SERVER['REQUEST_URI'];
+    if (strpos($href, 'wp-admin') !== false) return;
+    
+	$referer = $_SERVER['HTTP_REFERER'];
+	$host = $_SERVER['REMOTE_ADDR'];
+	$ident = $_SERVER['REMOTE_IDENT'];
+	$timeStamp = date("d/M/Y:H:i:s O");
+	$reqType = $_SERVER['REQUEST_METHOD'];
+	$servProtocol = $_SERVER['SERVER_PROTOCOL'];
+	$userAgent = $_SERVER['HTTP_USER_AGENT'];
+	$statusCode = "200";
+	$size = 0;
+
+	if ($host == "") $host = "-";
+	if ($ident == "") $ident = "-";
+	if ($auth == "") $auth = "-";
+	if ($reqType == "") $reqType = "-";
+	if ($servProtocol == "") $servProtocol = "-";
+
+	$referer = '"' . $referer . '"';
+
+	$userAgent = '"' . $userAgent . '"';
+
+	$clfString = $host." ".$ident." ".$auth." [".$timeStamp."] \"".$reqType." ".$href." ".$servProtocol."\" ".$statusCode." ".
+	  $size . " " . $referer . " " . $userAgent . "\r\n";
+
+	$logFile = dirname(__FILE__) . '/../../logs/access-' . date("Y-m") . ".log";
+
+	$fileWrite = fopen($logFile, 'a');
+
+	//flock($fileWrite, LOCK_SH);
+	fwrite($fileWrite, $clfString);
+	//flock($fileWrite, LOCK_UN);
+	fclose($fileWrite);
+}
+
+add_action('activate_access-logs/plugin.php', 'aal_activate');
+add_action('init', 'aal_init');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== Access Logs ===
+Tags: logs
+Requires at least: 2.1
+Tested up to: 2.3.3
+Stable tag: 1.0
+Donate link: http://www.satollo.com/english/donate
+Contributors: none
+
+Access Logs generate a access log file in Apache combined format rotated monthly.
+
+== Description ==
+
+Access Logs generate an access log file (rotated monthly) with access data in the
+folder wp-content/logs. Those files can be processed with common logs analyzers, like
+Webalizer.
+
+You have to protect the folder with an .htaccess file, otherwise anyone can download you logs.
+
+The logs can grow very quickly, pay attention to your disk space.
+
+== Installation ==
+
+Upload the plugin folder into your "wp-content/plugins"
+Log in to Wordpress Administration area, choose "Plugins" from the main menu, find "Access Logs", 
+and click the "Activate" button.
+
+No option are available by now. 
+
+To stop the logs, simple deactivate the plugin. The data collected will not be loosed.
+
+== Frequently Asked Questions ==
+
+No questions have been asked.
+
+== Screenshots ==
+
+No screenshots are available.
